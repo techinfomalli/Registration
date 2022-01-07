9001ef950c0135ee971e1b10d0c2d105a07508b5
DESKTOP-TIOVNGP\USER <techinfomalli@gmail.com>
2022-01-07 10:36:04 +0530

    added smtp mail and logs

--- src/main/java/com/malli/Registraion/service/UserService.java
+++ src/main/java/com/malli/Registraion/service/UserService.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,8 +24,10 @@ public class UserService {
 	@Autowired
 	EmailUtils emaiUtils;
 	
+	 Logger logger = LoggerFactory.getLogger(UserService.class);
+	
 	public List<User> findAllUsers() {
-
+		logger.info("***findAllUsers started***");
 		List<User> users = new ArrayList();
 		List<UserEntity> usreEntity = userRepo.findAll();
 		for (UserEntity entity : usreEntity) {
@@ -31,31 +35,38 @@ public class UserService {
 			BeanUtils.copyProperties(entity, user);
 			users.add(user);
 		}
-
+		logger.info("***End of started***");
 		return users;
 
 	}
 
 	public String deleteUser(Integer userId) {
+		logger.info("***User deletion started***");
+		logger.info("userId {}",userId);
 		userRepo.deleteById(userId);
+		logger.info("***User deletion compleated***");
 		return "Deleted successfully.";
 
 	}
 
 	public String createUser(User user) {
+		logger.info("***User created started***");
+		logger.info("User {}",user);
 		UserEntity entity = new UserEntity();
 		BeanUtils.copyProperties(user, entity);
+		entity.setUserAccStatus("LOCKED");
 		entity.setUserPwd(generateTempPwd());
 		UserEntity userEntity=userRepo.save(entity);
 		if(userEntity.getUserId()!=null) {
 			emaiUtils.sendEmail("Subject","body",userEntity.getUserEmail());
 		}
-
+		logger.info("***End ofcreateUser ***");
 		return "User created successfully.";
 
 	}
 
 	private String generateTempPwd() {
+		logger.info("*entered to generateTempPwd");
 		String tempPwd = null;
 		int leftLimit = 48; // numeral '0'
 		int rightLimit = 122; // letter 'z'
@@ -65,7 +76,7 @@ public class UserService {
 		tempPwd = random.ints(leftLimit, rightLimit + 1).filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
 				.limit(targetStringLength)
 				.collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
-
+		logger.info("*End generateTempPwd");
 		return tempPwd;
 	}
 }
--- src/main/java/com/malli/Registraion/utils/EmailUtils.java
+++ src/main/java/com/malli/Registraion/utils/EmailUtils.java
@@ -2,17 +2,25 @@ package com.malli.Registraion.utils;
 
 import javax.mail.internet.MimeMessage;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import com.malli.Registraion.service.UserService;
 @Service
 public class EmailUtils {
 	
 	@Autowired
 	private JavaMailSender mailSender;
+	
+	 Logger logger = LoggerFactory.getLogger(UserService.class);
 
 	public boolean sendEmail(String subject, String body, String to) {
+		logger.info("***sendEmail started***");
+		logger.info("subject,body,to {},{},{}",subject,body,to);
 		MimeMessage mimeMessage = mailSender.createMimeMessage();
 		try {
 			MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
